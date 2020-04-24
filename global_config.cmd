162416c9d8df0ee39ece904af74262f78045a7fe
Alex Sobhani <bsobhani@xf28idc-ioc1.cs.nsls2.local>
2020-04-24 17:52:11 -0400

    adding iocInit to global config file, as well as records.dbl

--- global_config.cmd
+++ global_config.cmd
@@ -1,5 +1,8 @@
 set_savefile_path("${TOP}/as","/save")
 set_requestfile_path("${TOP}/as","/req")
+iocInit()
+dbl > ${TOP}/records.dbl
+system "cp ${TOP}/records.dbl /cf-update/$HOSTNAME.$IOCNAME.dbl"
 #system("install -m 777 -d ${TOP}/as/save")
 #system("install -m 777 -d ${TOP}/as/req")
 makeAutosaveFiles()
