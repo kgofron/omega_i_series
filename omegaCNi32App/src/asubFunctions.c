49f5fc5b9e3b7aa8a1e655ddc6b266bd6cac4ae5
Alex Sobhani <bsobhani@bnl.gov>
2020-04-28 19:42:46 -0400

    testing removing calculation

--- omegaCNi32App/src/asubFunctions.c
+++ omegaCNi32App/src/asubFunctions.c
@@ -94,9 +94,9 @@ static long timeElapsed(aSubRecord *prec)
 {
 	epicsTimeStamp t;
 	epicsTimeGetCurrent(&t);
-	long t_diff = t.secPastEpoch - ((epicsTimeStamp*)prec->a)->secPastEpoch;
+	//long t_diff = t.secPastEpoch - ((epicsTimeStamp*)prec->a)->secPastEpoch;
 	printf("here\n");
-	printf("%d\n", t_diff);
+	//printf("%d\n", t_diff);
 	//*((double*)prec->vala) = t_diff;
 	return 0;
 }
