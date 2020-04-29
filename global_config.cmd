59935cd744d795da6090a471705a3eb6f9d928f4
Alex Sobhani <bsobhani@bnl.gov>
2020-04-29 19:01:51 -0400

    adding default values for PID and adding PID to autosave

--- global_config.cmd
+++ global_config.cmd
@@ -1,6 +1,7 @@
 dbLoadRecords("db/alarm_summary.db","Sys=${Sys}")
 set_savefile_path("${TOP}/as","/save")
 set_requestfile_path("${TOP}/as","/req")
+set_pass0_restoreFile("info_positions.sav")
 set_pass1_restoreFile("info_settings.sav")
 iocInit()
 #system("install -m 777 -d ${TOP}/as/save")
